53b1df4c15fc08032019af0b20d14be729d2b931
Kacper Nalepa <kacper.nlpa@gmail.com>
2025-10-17 00:13:12 +0200

    feat: implement JobDetails page with responsive layout and styling; add job fetching logic and display job information

--- client/src/pages/JobDetails.tsx
+++ client/src/pages/JobDetails.tsx
@@ -1,9 +1,56 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import AppButton from "@/components/AppButton";
 import SectionContainer from "@/components/SectionContainer";
+import type { Job } from "@/types/types";
+import { jobsData, assets } from "../assets/assets";
+import "./JobDetails.css";
 
 const JobDetails = () => {
+    const [job, setJob] = useState<Job | null>(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        const fetchJob = async () => {
+            const job = jobsData.find((jobEl: Job) => jobEl._id === id);
+            setJob(job);
+        };
+        fetchJob();
+    }, [id]);
+
+    function handleApplyNow() {
+        console.log("Apply Now");
+    }
+
     return (
         <SectionContainer>
-            <h1>Job Details</h1>
+            <div className="top-container">
+                <div className="top-container-details">
+                    <div className="top-container-img">
+                        <img src={job?.companyId.image} alt={job?.companyId.name} />
+                    </div>
+                    <div className="top-container-details-content">
+                        <h1>{job?.title}</h1>
+                        <div className="top-container-info">
+                            <div>
+                                <img src={assets.location_icon} alt="Location" />
+                                <span>{job?.location}</span>
+                            </div>
+                            <div>
+                                <img src={assets.money_icon} alt="Salary" />
+                                <span>{job?.salary}</span>
+                            </div>
+                            <div>
+                                <img src={assets.suitcase_icon} alt="Experience" />
+                                <span>{job?.companyId.name}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="top-container-actions">
+                    <AppButton onClick={handleApplyNow}>Apply Now</AppButton>
+                </div>
+            </div>
         </SectionContainer>
     );
 };
